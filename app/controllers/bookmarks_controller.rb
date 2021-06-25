0dce3666076f3ad77f71a6021c4e9ed1b2a859a4
Glenn Gunter <gunterglenn33@gmail.com>
2021-06-25 18:20:27 +0200

    app workingç

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,8 +17,9 @@ class BookmarksController < ApplicationController
 
   def destroy
    @bookmark = Bookmark.find(params[:id])
+   list = @bookmark.list
    @bookmark.destroy
-   redirect_to list_path(List.find(params[:list_id]))
+   redirect_to list_path(list)
   end
 
   private
